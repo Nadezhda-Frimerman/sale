0eacdb7ba1cdc476827241fc9531819d7d6e80d3
DariaLokt <d.lokteeva@yandex.ru>
2025-10-29 14:47:50 +0500

    Сделала CreateOrUpdateComment DTO

---
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateComment.java
@@ -0,0 +1,45 @@
+package ru.skypro.homework.dto;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+public class CreateOrUpdateComment {
+    @NotNull
+    @Size(min = 8, max = 64)
+    private String text = "";
+
+    public CreateOrUpdateComment() {
+    }
+
+    public CreateOrUpdateComment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CreateOrUpdateComment that = (CreateOrUpdateComment) o;
+        return Objects.equals(text, that.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(text);
+    }
+
+    @Override
+    public String toString() {
+        return "CreateOrUpdateComment{" +
+                "text='" + text + '\'' +
+                '}';
+    }
+}
