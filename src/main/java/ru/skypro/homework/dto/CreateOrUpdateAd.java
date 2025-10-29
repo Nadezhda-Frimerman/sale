ecc4384871ce6f56f96d6326f16f1a5321513982
DariaLokt <d.lokteeva@yandex.ru>
2025-10-29 14:43:17 +0500

    Сделала CreateOrUpdateAd DTO

---
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateAd.java
@@ -0,0 +1,70 @@
+package ru.skypro.homework.dto;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+public class CreateOrUpdateAd {
+    @Size(min = 4, max = 32)
+    private String title = "";
+    @Min(value = 0)
+    @Max(value = 10000000)
+    private Integer price = 0;
+    @Size(min = 8, max = 64)
+    private String description = "";
+
+    public CreateOrUpdateAd() {
+    }
+
+    public CreateOrUpdateAd(String title, Integer price, String description) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CreateOrUpdateAd that = (CreateOrUpdateAd) o;
+        return Objects.equals(title, that.title) && Objects.equals(price, that.price) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, price, description);
+    }
+
+    @Override
+    public String toString() {
+        return "CreateOrUpdateAd{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
