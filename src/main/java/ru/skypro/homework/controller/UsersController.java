78a3a7695ac1883bb456937193ea80a557a54462
DariaLokt <d.lokteeva@yandex.ru>
2025-10-29 17:34:26 +0500

    Сделала UsersController, но *

---
+++ src/main/java/ru/skypro/homework/controller/UsersController.java
@@ -0,0 +1,42 @@
+package ru.skypro.homework.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.NewPassword;
+import ru.skypro.homework.dto.UpdateUser;
+import ru.skypro.homework.dto.User;
+import ru.skypro.homework.service.UsersService;
+
+@Slf4j
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UsersController {
+    private final UsersService usersService;
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @PostMapping("/set_password")
+    public void setPassword(@RequestBody NewPassword newPassword) {
+    }
+
+    @GetMapping("/me")
+    public User getUser() {
+        return new User();
+    }
+
+    @PatchMapping("/me")
+    public UpdateUser updateUser(@RequestBody UpdateUser updateUser) {
+        return new UpdateUser();
+    }
+
+    @PatchMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void updateUserImage(@RequestParam("image") MultipartFile image) {
+    }
+}
