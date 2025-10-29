805f97909a6ccd1b69ab98f2d1c56ff8bdf2367d
DariaLokt <d.lokteeva@yandex.ru>
2025-10-29 15:31:18 +0500

    Сделала Ads DTO

---
+++ src/main/java/ru/skypro/homework/dto/Ads.java
@@ -0,0 +1,54 @@
+package ru.skypro.homework.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Ads {
+    private Integer count = 0;
+    private List<Ad> results = new ArrayList<>();
+
+    public Ads() {
+    }
+
+    public Ads(Integer count, List<Ad> results) {
+        this.count = count;
+        this.results = results;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public List<Ad> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Ad> results) {
+        this.results = results;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Ads ads = (Ads) o;
+        return Objects.equals(count, ads.count) && Objects.equals(results, ads.results);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(count, results);
+    }
+
+    @Override
+    public String toString() {
+        return "Ads{" +
+                "count=" + count +
+                ", results=" + results +
+                '}';
+    }
+}
